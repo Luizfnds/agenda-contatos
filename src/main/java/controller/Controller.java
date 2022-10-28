fc0fb39fb0f1aec339edddf90b25c21cf303f588
Luizfnds <luizeduardofnds@gmail.com>
2022-10-28 16:44:18 -0300

    DAO

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -7,16 +7,20 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import model.DAO;
+
 
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+	private DAO dao = new DAO();
+	
     public Controller() {
-        super();
+        
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		dao.testaConexao();
 	}
 
 }
--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -1,5 +1,38 @@
 package model;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DAO {
+	/** Módulo de conexão **/
+	
+	// Parâmetros de conexão
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://127.0.0.1:3306/dbagenda";
+	private String user = "root";
+	private String password = "3385";
+
+	// Método de conexão
+	private Connection conectar() {
+		Connection con = null;
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password);
+			return con;
+		} catch(Exception e){
+			System.out.println(e);
+			return null;
+		}
+	}
+	
+	public void testaConexao() {
+		try {
+			Connection con = conectar();
+			System.out.println(con);
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
